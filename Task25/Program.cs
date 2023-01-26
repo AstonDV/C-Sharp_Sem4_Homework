ef3635213ac0f4779a7cafb321dc217979638577
AstonDV <astondv.kms@gmail.com>
2023-01-27 00:00:10 +1000

    End task 25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Нельзя использовать библиотеку Math!
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int InputNum(string text)
+{
+    Console.Write($"{text}: ");
+    int tempNum = int.Parse(Console.ReadLine()!);
+    return tempNum;
+}
+
+int Exponentiation(int number, int degree)
+{
+    int tempExpo = 1;
+    for (int i = 0; i < degree; i++)
+    {
+        tempExpo *= number;
+    
+    }
+    return tempExpo;
+}
+
+void main()
+{
+    Console.Clear();
+    int a = InputNum("Введите число A");
+    int b = InputNum("Введите число B");
+    Console.Write($"Результат возведения числа {a} в натуральную степень {b} равен {Exponentiation(a, b)}");
+}
+
+main();
