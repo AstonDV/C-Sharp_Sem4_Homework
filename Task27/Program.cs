fe5ad82b3f1b248817fc590f80aec035e8652dce
AstonDV <astondv.kms@gmail.com>
2023-01-27 00:16:16 +1000

    End task 27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int InputNum(string text)
+{
+    Console.Write($"{text}: ");
+    int tempNum = int.Parse(Console.ReadLine()!);
+    return tempNum;
+}
+
+int SumNumbers (int n)
+{
+    int sum = 0;
+    if (n < 0) n *= -1;
+    for (int i = 0; n > 0; i++)
+    {
+        sum = n % 10 + sum;
+        n /= 10;
+    }
+    return sum;
+}
+
+void main()
+{
+    Console.Clear();
+    int check = InputNum("Введите ваше число");
+    Console.WriteLine($"Сумма цифр в введенном числе {check} равна: {SumNumbers(check)}");
+}
+
+main();
