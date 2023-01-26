8788e2a8780db8366c34cbadf551f3369d4368cc
AstonDV <astondv.kms@gmail.com>
2023-01-27 00:45:14 +1000

    End task 29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+string Input(string text)
+{
+    Console.Write($"{text}: ");
+    string numbers = Console.ReadLine();
+    return numbers;
+}
+
+string[] ArrayBuild(string BunchOfNumbers)
+{
+    char[] separators = new char[] {',', ' '};
+    string[] array = BunchOfNumbers.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    return array;
+}
+
+void main()
+{
+    Console.Clear();
+    string inputNum = Input("Введите восемь чисел через запятую или через пробел");
+    Console.Write($"[{String.Join(", ", ArrayBuild(inputNum))}]");
+}
+
+main();
